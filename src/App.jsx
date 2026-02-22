04662b2b8dd542df4bc931fd52dc41941b957de5
Nurmuhammed Orozbaev <orozbaevnurik2004@gmail.com>
2026-02-22 18:30:53 +0600

    Fix structure and add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Contacts from "./pages/Contacts";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <div style={{ padding: "20px" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<ProtectedRoute><Menu /></ProtectedRoute>} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-export default App;
